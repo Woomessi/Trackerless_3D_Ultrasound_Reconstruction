6ad3ea6b451f090037add5367a44f40ea635a5ed
Wu Hongrui <hongrui_wu@163.com>
2026-06-11 11:44:28 +0200

    260611

--- models/__init__.py
+++ models/__init__.py
@@ -9,7 +9,7 @@ from .online_framework import Online_Framework
 from .online_baseline_backbone import Online_Baseline_Backbone
 from .online_my_framework import Online_My_Framework
 from .online_jagged_backbone import Online_Jagged_Backbone
-from .online_finetuning_backbone import Online_Finetuning_Backbone
+from .online_finetuning_backbone_0611_1Loss import Online_Finetuning_Backbone
 
 __all__ = [
     'BaseModel', 'functional',
